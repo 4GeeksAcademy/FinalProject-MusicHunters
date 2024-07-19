8df6ba2cafab5fed923092841c48eee1bc2f1520
isramagix <israelclarinete@gmail.com>
2024-07-19 17:39:07 +0000

    Creados modelos Evento, Favoritos, Ticket Source y Price Tickets y ruta de Eventos GET

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Event, TicketsSource, PrecioTickets, Favoritos
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,11 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Event, db.session))
+    admin.add_view(ModelView(TicketsSource, db.session))
+    admin.add_view(ModelView(PrecioTickets, db.session))
+    admin.add_view(ModelView(Favoritos, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,26 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+import enum
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
+class EventType(enum.Enum):
+    concierto = "concierto"
+    festival = "festival"
+
+class GenereType(enum.Enum):
+    pop = "pop"
+    rock = "rock"
+    clasic = "clasic"
+    reggaeton = "reggaeton"
+    rap = "rap"
+    trap = "trap"
+    indie = "indie"
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)  # Campo requerido
-    email = db.Column(db.String(120), unique=True, nullable=False)     # Campo requerido
-    password = db.Column(db.String(200), nullable=False)                # Campo requerido
-    name = db.Column(db.String(120), nullable=True)                    # Campo opcional
-    last_name = db.Column(db.String(120), nullable=True)               # Campo opcional
-    phone = db.Column(db.String(120), nullable=True)                   # Campo opcional
-    address = db.Column(db.String(120), nullable=True)                 # Campo opcional
-    profile_picture = db.Column(db.String(120), nullable=True)         # Campo opcional
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(120), nullable=True)
+    last_name = db.Column(db.String(120), nullable=True)
+    phone = db.Column(db.String(120), nullable=True)
+    address = db.Column(db.String(120), nullable=True)
+    profile_picture = db.Column(db.String(120), nullable=True)
+
+    favoritos = db.relationship('Favoritos', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
-    
+
     def generate_password_hash(self, password):
         return bcrypt.generate_password_hash(password).decode('utf-8')
-    
+
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
@@ -47,6 +63,120 @@ class User(db.Model):
             "phone": self.phone,
             "address": self.address,
             "profile_picture": self.profile_picture
-            # No se serializa la contraseña por razones de seguridad
         }
 
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(120), nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    event_type = db.Column(db.Enum(EventType), nullable=False)
+    genere = db.Column(db.Enum(GenereType), nullable=False)
+
+    favoritos = db.relationship('Favoritos', backref='event', lazy=True)
+    precios = db.relationship('PrecioTickets', backref='event', lazy=True)
+
+    def __repr__(self):
+        return f'<Event {self.name}>'
+
+    def create_new_event(self, name, description, date, location, event_type, genere):
+        self.name = name
+        self.description = description
+        self.date = date
+        self.location = location
+        self.event_type = event_type
+        self.genere = genere
+
+        db.session.add(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "date": self.date,
+            "location": self.location,
+            "event_type": self.event_type.value,  # Serializa el valor del Enum
+            "genere": self.genere.value,          # Serializa el valor del Enum
+            "precios": [precio.serialize() for precio in self.precios]  # Agrega los precios
+        }
+
+
+class TicketsSource(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    web_url = db.Column(db.String(250), nullable=False)
+
+    precios = db.relationship('PrecioTickets', backref='source', lazy=True)
+
+    def __repr__(self):
+        return f'<TicketsSource {self.name}>'
+
+    def create_new_tickets_source(self, name, web_url):
+        self.name = name
+        self.web_url = web_url
+
+        db.session.add(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "web_url": self.web_url
+        }
+
+
+class PrecioTickets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    source_id = db.Column(db.Integer, db.ForeignKey('tickets_source.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    # url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<PrecioTickets {self.price}>'
+
+    def create_new_precio_ticket(self, event_id, source_id, price, url):
+        if event_id is None or source_id is None:
+            raise ValueError("Event ID and Source ID cannot be null")
+        self.event_id = event_id
+        self.source_id = source_id
+        self.price = price
+        # self.url = url
+
+        db.session.add(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            # # "url": self.url,
+            "source": self.source.serialize()  # Incluye la información de la fuente de tickets
+        }
+
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Favoritos {self.id}>'
+
+    def create_new_favorito(self, user_id, event_id):
+        self.user_id = user_id
+        self.event_id = event_id
+
+        db.session.add(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "event_id": self.event_id
+        }
